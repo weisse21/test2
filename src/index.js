e38aaa44d10a0afbd2e2208eb80770130527139b
weisse21 <weissenboeck.mi@gmail.com>
2024-10-24 12:53:28 +0200

    Navbar

--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import Dom from "react-dom"
+import Navbar from "./components/navbar";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = Dom.createRoot(document.getElementById("root"));
+const element = <h1>Navbar</h1>;
+root.render(element)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
